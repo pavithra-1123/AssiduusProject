71128a9c02493f70627cbf5ca6059583ccad3a55
pavithra-1123 <pavithrarssp@gmail.com>
2023-11-11 23:50:37 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route,  Routes } from 'react-router-dom';
-import { HomePage } from "./1_Task_Navbar";
-import { ListPage1 } from "./1_Task_ListDetails";
-import { Home1 } from './1_Task_Home';
+import { HomePage } from "../../zohoproject/src/1_Task_Navbar";
+import { ListPage1 } from "../../zohoproject/src/1_Task_ListDetails";
+import { Home1 } from '../../zohoproject/src/1_Task_Home';
 import {  ChartSiderBar } from './ChartPage';
 import Navbar, { ChartNavPage } from './Chart_NavPage';
 
@@ -11,16 +11,10 @@ import Navbar, { ChartNavPage } from './Chart_NavPage';
 function App() {
   return(
     <>
-    {/* <BrowserRouter>
-    <HomePage/>
-    <Routes>
-      <Route path='Home1' exact element={<Home1/>} />
-      <Route path='list1' exact element={<ListPage1/>}/>
-    </Routes>
-    </BrowserRouter> */}
+   
     <ChartNavPage/>
   
-    {/* <ChartSiderBar/> */}
+    <ChartSiderBar/>
     </>
   );
 }
--- src/Chart_NavPage.js
+++ src/Chart_NavPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from "react-bootstrap";
-import { alignPropType } from 'react-bootstrap/esm/types';
-import { NavLink } from 'react-router-dom';
+// import { alignPropType } from 'react-bootstrap/esm/types';
+// import { NavLink } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import Face3Icon from '@mui/icons-material/Face3';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import './Chart_style_Page.css';
 
 export const ChartNavPage=()=>
@@ -13,10 +15,49 @@ export const ChartNavPage=()=>
        
             <Navbar bg="light" expand="lg">
                 <div className='sear'  >
-                    <label className='sear'></label>
-                    <input type='search' className='' /><SearchIcon className='ms-1'></SearchIcon>  
-                    <label className='notify'></label>
-                    <NotificationsIcon></NotificationsIcon> 
+                    <label className='sear form-label' ></label>
+                    <input type='search' className='form-control' /><SearchIcon className='ms-1'></SearchIcon>  
+                    <label className=''></label>
+                    <NotificationsIcon className='ms-5'></NotificationsIcon> 
+                    <Face3Icon className='ms-5'></Face3Icon>
+                    <ArrowDropDownIcon className='ms-5'></ArrowDropDownIcon>
+             
+                    <div class="dropdown">
+        <a
+          class="dropdown-toggle d-flex align-items-center hidden-arrow"
+          href="#"
+          id="navbarDropdownMenuAvatar"
+          role="button"
+          data-mdb-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <img 
+          
+          />
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            class="rounded-circle"
+            height="25"
+            alt="Black and White Portrait of a Man"
+            loading="lazy"
+          />
+        </a>
+        <ul
+          class="dropdown-menu dropdown-menu-end"
+          aria-labelledby="navbarDropdownMenuAvatar"
+        >
+          <li>
+            <a class="dropdown-item" href="#">My profile</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Settings</a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+
                 </div>
 
                 <NavbarBrand className=' ' type='search'> </NavbarBrand>
