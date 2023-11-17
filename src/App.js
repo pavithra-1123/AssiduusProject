8a25990a8a966437075644c47e6975db617b5e86
pavithra-1123 <pavithrarssp@gmail.com>
2023-11-17 19:18:02 +0530

    go

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 // import { Chart } from 'chart.js/dist';
 import {  ChartSiderBar } from './ChartPage';
 import  { ChartNavPage } from './Chart_NavPage';
-import { Chart112, My_Chartpage} from './Chart_line';
-import LineChart from './chart';
+import { My_Chartpage} from './Chart_line';
+
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 
@@ -15,7 +15,6 @@ export function App() {
     <ChartSiderBar/>
       <Routes>
         <Route path='view' exact element={<My_Chartpage/>} />
-       
       </Routes>
       </BrowserRouter>
  
