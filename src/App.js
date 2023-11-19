e2fdbfec1084f1304e8e6edc516069f237c2e7ea
pavithra-1123 <pavithrarssp@gmail.com>
2023-11-19 22:18:51 +0530

    changess assiduus

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import { ChartNavPage } from './Chart_NavPage';
+import { My_Chartpage} from './Chart_line';
+import {  ChartSiderBar } from './ChartPage';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <div class="wrapper" style={{
+        display: "flex",
+        height: "100vh"
+      }}>
+      
+      <div class="sidebar">
+      
+      <ChartSiderBar/>
+      </div>
+      <div class="content">
+      <ChartNavPage/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='view' exact element={<My_Chartpage/>} />
+        
+      </Routes>
+      </BrowserRouter>
+      </div>
+   </div>
+
+
+      
+    </>
+  );
+}
+
+export default App;
